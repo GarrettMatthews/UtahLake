bf003cc2fd023ea9777f19ac1ca175748b7e417b
Garrett Matthews <garrettematthews@gmail.com>
2019-01-24 13:28:03 -0700

    Starting data analysis

---
+++ ulake.R
@@ -0,0 +1,15 @@
+#Entering the data in
+
+read.csv("UL_BiologicalData.csv") -> df
+
+#Seperating out columns I think will be the focus of interpretation
+tax_name = df$SubjectTaxonomicName
+date = df$ActivityStartDate
+
+#Changing the date column to be a date class
+as.Date(date, tryFormats = c("%m-%d-%Y", "%m/%d/%Y"), optional = TRUE) -> date
+
+#Seperating out this column to act as a location identifier.
+#Visit https://maps.waterdata.usgs.gov/mapper/index.html for USDS codes
+# and https://enviro.deq.utah.gov/ for UTAHDWQ codes
+df$MonitoringLocationIdentifier -> local_code
